afc2a5563e020164844d749ef9b016f161790faa
charles ntungane <ntunganecharles@gmail.com>
2018-12-02 19:39:54 -0500

    initial commit

---
+++ src/main/java/au/com/riosoftware/JPABasicMappings.java
@@ -0,0 +1,15 @@
+package au.com.riosoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@ComponentScan("au.com.riosoftware")
+class JPABasicMappings {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPABasicMappings.class, args);
+	}
+
+// hello this is a comment
+}
